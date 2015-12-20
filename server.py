c81a144cccdebdec5e7089803a8f6efe9f1cc1e6
abhishekjiitr <abhi2254015@gmail.com>
2015-12-21 00:16:57 +0530

    Started part2

--- server.py
+++ server.py
@@ -1,5 +1,35 @@
 #!/usr/bin/env python
 # Simple HTTP Server in python
 
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-import os
+import time
+import BaseHTTPServer
+
+HOST_NAME = 'example.com'
+PORT_NUMBER = 9000 # Can change this
+
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_HEAD(s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+    def do_GET(s):
+        # Respomd to a GET request
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+        s.wfile.write("<html><head><title>The Random Website</title></head></html>")
+        s.wfile.write("<body><p>This is a test.</p>")
+
+        s.wfile.write("<p>You accessed path: %s</p>" % s.path)
+        s.wfile.write("</body></html>")
+
+if __name__ == '__main__':
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("Interrupted by keyboard")
+    httpd.server_close()
+    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
